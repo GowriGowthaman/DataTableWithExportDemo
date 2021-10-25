211b527fba8b28410da15aa58ed6693a910bd180
GowriGowthaman <gowri@inarvo.com>
2021-10-25 20:05:26 +0530

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module "jszip/dist/jszip";
+declare module "pdfmake/build/pdfmake";
+declare module "datatables.net-dt/js/dataTables.dataTables";
+declare module "datatables.net-buttons-dt/js/buttons.dataTables";
+declare module "datatables.net-buttons/js/buttons.colVis";
+declare module "datatables.net-buttons/js/buttons.html5";
+declare module "datatables.net-buttons/js/buttons.print";
